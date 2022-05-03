4797c0e91a247c675c20fce555d14f6a8dc08936
Jared Cosulich <jared.cosulich@gmail.com>
2022-05-02 21:18:14 -0400

    confirm mint and gated content

--- components/NFTMintAndCheckTokenDemo.jsx
+++ components/NFTMintAndCheckTokenDemo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import {
   TWFullScreen,
   TWCenteredContent,
@@ -13,10 +13,40 @@ import { TWButton } from '.';
 const NFTMintAndCheckTokenDemo = () => {  
   const [user, setUser] = useState();
   const [message, setMessage] = useState();
+  const [nft, setNft] = useState();
+  const [gated, setGated] = useState(false)
+
+  const checkNfts = useCallback(async () => {
+    const nfts = await lib.walletContents(user.wallet.address)
+    for (const nft of nfts) { 
+      if (nft.token_address.toLowerCase() === ContractInfo.address.toLowerCase()) {
+        setNft(nft);
+        break;
+      }
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (!user) {
+      const activeUser = lib.activeUser();
+      if (activeUser) {
+        console.log("Active User: ", activeUser)
+        setUser(activeUser);
+      }
+      return;
+    }
+
+    const fetchUser = async () => {
+      setUser(user);
+      checkNfts();
+    };
+
+    fetchUser();
+  }, [user])
 
   const mint = () => {
     if (!user) return;
-    console.log("TRANSACT!")
+
     lib.transact({
       email: user.email,
       network: ContractInfo.network,
@@ -28,53 +58,112 @@ const NFTMintAndCheckTokenDemo = () => {
       onSigned: () => setMessage("Sending Minting Request..."),
       onSent: () => setMessage("Waiting For Confirmation..."),
       onComplete: () => setMessage("Minting Completed, Confirming Transaction..."),
-      onConfirmed: () => setMessage("Minting Complete!")
+      onConfirmed: () => {
+        setMessage(null)
+        checkNfts();
+      }
     })
+  }
+
+  const logout = () => {
+    lib.logout();
+    setNft(null);
+    setUser(null);
+    setGated(false)
+  }
+
+  const enter = () => {
+    setGated(true);
   }
 
   return (
     <TWFullScreen>
-      <TWCenteredContent>
-        <div className='text-center'>
-          <h2 className='text-xl pb-6'>
-            NFT Gated Content Demo
-          </h2>
-          {user ? (
-            <div className='py-6'>
-              <h3 className='text-lg'>
-                Hi {user.email}!
-              </h3>
-              <div className='py-3'>
-                Address: {user.wallet.address}
-              </div>
-              <div className='py-3'>
-                <div className='py-3'>
-                  To join our community you need to mint a token!
-                </div>  
+      <div className='container mx-auto'>
+        { user && (
+          <div className='border-b px-3 py-6 flex justify-between'>
+            <div>NFT Gated Content Demo</div>
+            <div>
+              <TWButton
+                onClick={logout}
+              >
+                Logout
+              </TWButton>
+            </div>
+          </div>
+        )}
+        <TWCenteredContent>
+          <div className='text-center'>
+            {user ? (
+              <div className='py-24'>
+                {gated ? (
+                  <div className='text-center text-2xl'>
+                    GATED CONTENT
+                  </div> 
+                ) : (
+                  <>
+                    <h3 className='text-lg'>
+                      Hi {user.email}!
+                    </h3>
+                    <div className='py-3'>
+                      Address: {user.wallet.address}
+                    </div>
+                    <div className='py-3'>
+                      {nft ? (
+                        <>
+                          <div className='text-center flex items-center justify-center'>
+                            <img
+                              src={JSON.parse(nft.metadata || "{}").image || "https://testnets.opensea.io/static/images/placeholder.png"}
+                              alt={nft.name}
+                              className='object-contain w-60 h-40'
+                            />
+                          </div>
+                          <div className='py-3'>
+                            You've minted a token. Welcome to the community!
+                          </div>
+                          <TWButton onClick={enter}>
+                            Enter Community
+                          </TWButton>
+                        </>
+                      ) : (
+                        <>
+                          <div className='py-3'>
+                            To join our community you need to mint a token!
+                          </div>  
 
-                <TWButton
-                  onClick={mint}
-                >
-                  Mint!
-                </TWButton>
+                          <TWButton
+                            onClick={mint}
+                          >
+                            Mint!
+                          </TWButton>
+                        </>
+                      )}
+                      
 
-                {message && (
-                  <div className='pt-3'>
-                    {message}
-                  </div>
-                )}
+                      {message && (
+                        <div className='pt-3'>
+                          {message}
+                        </div>
+                      )}
+                    </div>
+                  </>
+                )}                
+              </div> 
+            ) : (
+              <div className='py-36'>
+                <h2 className='text-xl pb-6'>
+                  NFT Gated Content Demo
+                </h2>
+                <tailwind.SignInButton 
+                  className='bg-gray-200 px-3 py-1 rounded-lg' 
+                  onSignIn={setUser}
+                />
               </div>
-            </div> 
-          ) : (
-            <tailwind.SignInButton 
-              className='bg-gray-200 px-3 py-1 rounded-lg' 
-              onSignIn={setUser}
-            />
-          )}
-          
-          {/* <WalletHasTokenFlowAPI contractMetadata={ContractInfo}/> */}
-        </div>
-      </TWCenteredContent>
+            )}
+            
+            {/* <WalletHasTokenFlowAPI contractMetadata={ContractInfo}/> */}
+          </div>
+        </TWCenteredContent>
+      </div>
     </TWFullScreen>
   )
 }
